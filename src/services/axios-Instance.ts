dbb6d2c247a5bce50ccb151fa2c056ad8c70d84f
Damir Tagankhozhaev <155202834+mobkukariku@users.noreply.github.com>
2025-01-10 18:12:43 +0500

    Update axios-Instance.ts

--- src/services/axios-Instance.ts
+++ src/services/axios-Instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://async-race-server-production-bf9e.up.railway.app",
     headers: {
         "Content-Type": "application/json",
     },
